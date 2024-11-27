84c029acd58d77ad77218ee605e8b8405ee7ed12
Bruno Santos <bruno.desantanasantos@gm.com>
2024-11-27 18:58:20 -0300

    Change: Added new 'add_task' and 'update_task' functions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 from os.path import exists
 
@@ -24,20 +25,50 @@ class FileHandler:
             return tasks[-1].get('id')
         return 0
 
-    def add_task(self, task):
-        tasks = self._read_tasks()
-        task_id = self.get_last_id(tasks) + 1
-        task_data = {
-            'id': task_id,
-            'description': task.description,
-            'stats': task.status,
-            'created_at': task.created_at,
-            'updated_at': task.updated_at
-        }
-        tasks.append(task_data)
-        self._write_tasks(tasks)
+    def add_task(self, task) -> bool:
+        try:
+            tasks = self._read_tasks()
+            task_id = self.get_last_id(tasks) + 1
+            task_data = {
+                'id': task_id,
+                'description': task.description,
+                'status': task.status,
+                'created_at': task.created_at,
+                'updated_at': task.updated_at
+            }
+            tasks.append(task_data)
+            self._write_tasks(tasks)
 
-    def delete_task(self, task_id):
-        tasks = self._read_tasks()
-        tasks = [task for task in tasks if task.get('id') != task_id]
-        self._write_tasks(tasks)
+            return True
+
+        except Exception as e:
+            return False
+
+    def delete_task(self, task_id) -> bool:
+        try:
+            tasks = self._read_tasks()
+            tasks = [task for task in tasks if task.get('id') != task_id]
+            self._write_tasks(tasks)
+            return True
+
+        except Exception as e:
+            return False
+
+    def update_task(self, task_id, update: dict) -> bool:
+        key = list(update.keys())[0]
+        value = update[key]
+        try:
+            tasks = self._read_tasks()
+            for task in tasks:
+                if task.get('id') == task_id:
+                    task[key] = value
+                    task['updated_at'] = now()
+                    self._write_tasks(tasks)
+                    return True
+            return False
+
+        except Exception as e:
+            return False
+
+def now():
+    return datetime.now().isoformat()
