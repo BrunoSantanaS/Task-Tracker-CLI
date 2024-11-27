d57667d4291f59735a86242dded68e1c12462198
Bruno Santos <bruno.desantanasantos@gm.com>
2024-11-27 18:59:04 -0300

    Change: incremented new functions to the Task class

--- task/__init__.py
+++ task/__init__.py
@@ -1,7 +1,7 @@
-from utils import FileHandler
+from utils import FileHandler, now
 
 class Task:
-    def __init__(self, description, status, created_at, updated_at):
+    def __init__(self, description, status, created_at = now(), updated_at = now()):
         self.description = description
         self.status = status
         self.created_at = created_at
@@ -11,5 +11,36 @@ class Task:
         return f'{self.description} - {self.status}'
 
     def create_task(self):
-        # Receive the task object and save it to a file
-        return None
+        file_handler = FileHandler()
+        return file_handler.add_task(self)
+
+    def delete_task(self, task_id):
+        file_handler = FileHandler()
+        return file_handler.delete_task(task_id)
+
+    def update_task(self, task_id, update_data: dict):
+        file_handler = FileHandler()
+        return file_handler.update_task(task_id, update_data)
+
+
+def get_task_by_id(id: int) -> Task:
+    file_handler = FileHandler()
+    tasks = file_handler._read_tasks()
+    for task in tasks:
+        if task.get('id') == id:
+            return Task(task.get('description'), task.get('status'), task.get('created_at'), task.get('updated_at'))
+    return None
+
+def get_tasks(filter_by: str = None) -> list:
+    file_handler = FileHandler()
+    tasks = file_handler._read_tasks()
+
+    if filter_by is None:
+        return tasks
+
+    response_tasks = []
+    for task in tasks:
+        if task.get('status') == filter_by:
+            response_tasks.append(task)
+
+    return response_tasks
